01b4e223bf50c92755979cc42e5c45d52c38ce00
Ben Elgar <me@benelgar.com>
2021-04-16 12:17:03 +0100

    Fix bad region name map

--- price-indexer/index-key.ts
+++ price-indexer/index-key.ts
@@ -9,12 +9,12 @@ export const regionNameMap = {
   'ap-northeast-1': 'Asia Pacific (Tokyo)',
   'ca-central-1': 'Canada (Central)',
 
-  'EU (Frankfurt)': 'eu-central-1',
-  'EU (Ireland)': 'eu-west-1',
-  'EU (London)': 'eu-west-2',
-  'EU (Milan)': 'eu-south-1',
-  'EU (Paris)': 'eu-west-3',
-  'EU (Stockholm)': 'eu-north-1',
+  'eu-central-1': 'EU (Frankfurt)',
+  'eu-west-1': 'EU (Ireland)',
+  'eu-west-2': 'EU (London)',
+  'eu-south-1': 'EU (Milan)',
+  'eu-west-3': 'EU (Paris)',
+  'eu-north-1': 'EU (Stockholm)',
 
   'me-south-1': 'Middle East (Bahrain)',
   'sa-east-1': 'South America (Sao Paulo)',
