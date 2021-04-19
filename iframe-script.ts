f627583aefd014f72365d6e6e5a91b5b079854b0
Ben Elgar <me@benelgar.com>
2021-04-19 16:19:32 +0100

    Flip conditions

--- iframe-script.ts
+++ iframe-script.ts
@@ -5,17 +5,17 @@ const renderPrice = async ({ type, hourlyCost }: InstanceInfo) => {
   document.body.style.display = 'unset';
 
   const priceSpan = document.querySelector('#deref-monthly-cost');
-  if (!priceSpan) {
-    doWarn('Price span not found');
-  } else {
+  if (priceSpan) {
     priceSpan.textContent = (hourlyCost * 730).toFixed(2);
+  } else {
+    doWarn('Price span not found');
   }
 
   const instanceTypeSpan = document.querySelector('#instance-type');
-  if (!instanceTypeSpan) {
-    doWarn('Instance type span not found');
-  } else {
+  if (instanceTypeSpan) {
     instanceTypeSpan.textContent = type;
+  } else {
+    doWarn('Instance type span not found');
   }
 };
 
