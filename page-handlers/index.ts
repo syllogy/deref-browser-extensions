a3f37cf47296273c7af3c0db597401c3795c73b7
Ben Elgar <me@benelgar.com>
2021-04-20 20:29:11 +0100

    Remove unnecessary return wrap

--- page-handlers/index.ts
+++ page-handlers/index.ts
@@ -1,10 +1,9 @@
 import { doPageHandler } from './common';
 
-export const handleAllPages = async () => {
-  await Promise.all(
+export const handleAllPages = async () =>
+  Promise.all(
     [
       (await import('./ec2-instance-wizard')).ec2InstanceWizard,
       (await import('./ec2-instance-list')).ec2InstanceList,
     ].map(doPageHandler),
   );
-};
