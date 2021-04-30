d8fcb456df3f99b0e78c435c39a89b113ac36ee5
John <srjohnmorrison@gmail.com>
2021-04-30 12:54:28 -0400

    Login CTA styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 const sizeClasses = {
   sm: 'py-1 px-2 text-xs',
   md: 'py-1 px-3 text-sm',
-  lg: 'py-2 px-4 text-base',
+  lg: 'py-2 px-6 text-base',
 };
 
 type Size = keyof typeof sizeClasses;
@@ -13,7 +13,7 @@ const variantClasses = {
   default:
     'rounded hover:bg-gray-100 border border-b-2 border-gray-200 hover:border-gray-300 text-black',
   primary:
-    'rounded bg-blue-500 hover:bg-blue-600 border border-b-2 border-blue-600 hover:border-blue-800 text-white',
+    'rounded bg-blue-500 hover:bg-blue-600 border border-b-2 border-blue-700 hover:border-blue-900 text-white',
 };
 
 type Variant = keyof typeof variantClasses;
