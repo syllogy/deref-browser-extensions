332b7db8ef881abd7f7e906a7666a7c8bec4ca41
Brandon Bloom <brandon@brandonbloom.name>
2021-05-03 10:19:01 -0700

    web-ext --start-url=...

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,7 +6,15 @@ set -xeuo pipefail
 # shellcheck disable=SC2064
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-(cd dist && web-ext run --verbose) &
+# Use local AWS CLI config to find a convenient login url.
+account_id="$(aws sts get-caller-identity --query 'Account' --output text)"
+if [[ -z "$account_id" ]]; then
+  start_url="https://signin.aws.amazon.com/console"
+else
+  start_url="https://${account_id}.signin.aws.amazon.com/console"
+fi
+
+(cd dist && web-ext run --verbose "--start-url=$start_url") &
 esbuild --bundle --target=firefox86,chrome88 --outdir='./dist/src' ./*.ts ./*.tsx --watch &
 
 wait
